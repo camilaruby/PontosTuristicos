6b6b107358a514f5ba91ec3f0e63a0800219a41d
Usuário Convidado <Guest@pa06micro21.local>
2017-12-19 10:45:47 -0200

    Initial Commit

---
+++ PontosTuristicos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PontosTuristicos
+//
+//  Created by Usuário Convidado on 19/12/17.
+//  Copyright © 2017 br.com.camila.fiap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
